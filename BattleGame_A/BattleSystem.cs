24df1b8d59dbeeea38a759e08e1e3a10852f2457
David Allison <dallison@conestogac.on.ca>
2022-09-22 21:48:53 -0400

    Add project files.

---
+++ BattleGame_A/BattleSystem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleGame_A
+{
+    public static class BattleSystem
+    {
+        //You will need to replace anything in <these> with the proper datatypes
+        //and variable names that work for your implementation
+
+        /*
+         public static <output type> RunBattle(<input types> <input variables>)
+        {
+        
+        }
+        */
+
+        /*
+        private static <output type> GenerateDefenseStub()
+        {
+
+        }
+        */
+
+        /*
+        private static <output type> GenerateAttackStub(<input types> <input variables>)
+        {
+
+        }
+        */
+    }
+}
---
+++ BattleGame_B/BattleSystem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleGame_B
+{
+    public static class BattleSystem
+    {
+        //You will need to replace anything in <these> with the proper datatypes
+        //and variable names that work for your implementation
+
+        /*
+         public static <output type> RunBattle(<input types> <input variables>)
+        {
+        
+        }
+        */
+
+        /*
+        private static <output type> GenerateDefense()
+        {
+
+        }
+        */
+
+        /*
+        private static <output type> GenerateAttackStub(<input types> <input variables>)
+        {
+
+        }
+        */
+    }
+}
