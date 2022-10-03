3e4b55f69bb7adc2dd6a3f4dff055c7e9a5e45b0
Hunter-Oswald <hunter-o@outlook.com>
2022-10-02 21:35:34 -0400

    battlegame_A complete

--- BattleGame_A/BattleSystem.cs
+++ BattleGame_A/BattleSystem.cs
@@ -11,25 +11,101 @@ namespace BattleGame_A
         //You will need to replace anything in <these> with the proper datatypes
         //and variable names that work for your implementation
 
-        /*
-         public static <output type> RunBattle(<input types> <input variables>)
+        static public void Main(String[] args)
         {
+            //
+            int bossHP = 300;
+            while (bossHP > 0) {
+                Console.WriteLine("The Boss has" + bossHP+ "HP Remaining!");
+                Console.WriteLine("Enter your next attack type (1 = F, 2 = W, 3 = G: ");
+                int attackType = Console.ReadLine();
+                if (attackType <= 0 || attackType > 3)
+                {
+                    Console.WriteLine("Invalid input.");
+                } else
+                {
+                    int damage = RunBattle(attackType);
+                    bossHP = bossHP - damage;
+                }
+            }
+        }
+
+        public static int RunBattle(int attackType)
+        {
+            // 1 = fire, 2 = water, 3 = grass
+            int defense = GenerateDefense();
+
+            int att = GenerateAttack(defense, attackType);
+            Console.WriteLine($"Attack's damage: {att}");
+
+            return att;
+        }
+
+        private static int GenerateDefense()
+        {
+            // 1 = fire, 2 = water, 3 = grass
+            Random rnd = new Random();
+            int defenseType = rnd.Next(1, 4);
+            return defenseType;
+
+        }
         
-        }
-        */
 
-        /*
-        private static <output type> GenerateDefenseStub()
+        
+        private static int GenerateAttack(int defenseType, int attackType)
         {
+        // attack power random from 1-20
+            Random rnd = new Random();
+            int attackPow = rnd.next(1, 21);
+
+            double elementMult = 1;
+
+            // 1 = fire, 2 = water, 3 = grass
+            if (attackType == 1 && defenseType == 3)
+            {
+                elementMult = 2;
+            } else if (attackType == 1 && defenseType == 2)
+            {
+                elementMult = 0.5;
+            } else if (attackType == 1 && defenseType == 1) {
+                elementMult = 1;
+            } else if (attackType == 2 && defenseType == 1)
+            {
+                elementMult = 2;
+            } else if (attackType == 2 && defenseType == 2)
+            {
+                elementMult = 1;
+            } else if(attackType == 2 && defenseType == 3)
+            {
+                elementMult = 0.5;
+            } else if (attackType == 3 && defenseType == 1)
+            {
+                elementMult = 0.5;
+            } else if(attackType == 3 && defenseType == 2)
+            {
+                elementMult = 2;
+            } else if(attackType == 3 && defenseType == 3)
+            {
+                elementMult = 1;
+            } else
+            {
+                elementMult = 1;
+            }
+
+            // critical strike chance
+            int crit = 1;
+            int critChance = rnd.Next(1, 21);
+
+            if(critChance == 20)
+            {
+                crit = 3;
+            }
+
+            int finalAttackPow = attackPow * elementMult * crit;
+            return finalAttackPow;
+
 
         }
-        */
-
-        /*
-        private static <output type> GenerateAttackStub(<input types> <input variables>)
-        {
-
-        }
-        */
+        
     }
 }
